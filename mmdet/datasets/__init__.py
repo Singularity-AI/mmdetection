b08b26f283964069dfa480a62fe751d55ae40318
gengxqa <57425108+gengxqa@users.noreply.github.com>
2020-05-18 17:03:29 +0800

    增加NEC数据注册 (#5)
    Co-authored-by: leowangzi <leowangzi@163.com>

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -9,11 +9,12 @@ from .voc import VOCDataset
 from .her2 import Her2Dataset  # for her2 detection
 from .mitosis import MitosisDataset  # for mitotic detection
 from .ki67 import KI67Dataset, KI67MaskDataset  # for ki-67 detection
+from .ki67NEC import KI67NECDataset  # for NEC detection
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 
 __all__ = [
-    'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'MitosisDataset', 'KI67Dataset', 'KI67MaskDataset',"Her2Dataset",
+    'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'MitosisDataset', 'KI67Dataset', 'KI67MaskDataset',"Her2Dataset",'KI67NECDataset',
     'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'build_dataloader', 'ConcatDataset', 'RepeatDataset', 'WIDERFaceDataset',
     'DATASETS', 'build_dataset'
